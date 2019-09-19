cba18f0e59921011d78f2e040016743fd2921690
Ishpreet Sekhon <issekhon@sfu.ca>
2019-09-18 18:05:16 -0700

    Added Network Test Scene
    Added network test scene to test network connection

---
+++ planetarycontrol/Assets/Scripts/PlayerNetworkObjectScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerNetworkObjectScript : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer == false) {
+			return;
+		}
+
+		CmdSpawnMyUnit ();
+	}
+
+	public GameObject playerUnitPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	[Command]
+	void CmdSpawnMyUnit() {
+		Debug.Log ("PlayerNetworkObject:: Spawning A Player Unit");
+		GameObject go = Instantiate (playerUnitPrefab);
+		NetworkServer.SpawnWithClientAuthority (go, connectionToClient);
+	}
+}
